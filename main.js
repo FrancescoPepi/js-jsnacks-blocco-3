b0e96e57d9928ea4caeec6d5dceb6a038916b2db
FrancescoPepi <francescopepi16@gmail.com>
2023-07-26 16:28:33 +0200

    ES 3

--- main.js
+++ main.js
@@ -1,26 +1,62 @@
-// Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+// // Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
 
-const PrimoNumero = parseInt(prompt("inserisci il primo numero"));
-const SecondoNumero = parseInt(prompt("inserisci il secondo numero"));
+// const primoNumero = parseInt(prompt("inserisci il primo numero"));
+// const secondoNumero = parseInt(prompt("inserisci il secondo numero"));
 
-let NumeroPiuGrande;
-let NumeroPiuPiccolo;
+// let numeroPiuGrande;
+// let numeroPiuPiccolo;
 
-if (PrimoNumero > SecondoNumero) {
-  NumeroPiuGrande = PrimoNumero;
-  NumeroPiuPiccolo = SecondoNumero;
-} else if (PrimoNumero < SecondoNumero) {
-  NumeroPiuGrande = SecondoNumero;
-  NumeroPiuPiccolo = PrimoNumero;
-} else {
-  alert("i numeri sono uguali");
+// // calcolo del più grande
+// if (primoNumero > secondoNumero) {
+//   numeroPiuGrande = primoNumero;
+//   numeroPiuPiccolo = secondoNumero;
+// } else if (primoNumero < secondoNumero) {
+//   numeroPiuGrande = secondoNumero;
+//   numeroPiuPiccolo = primoNumero;
+// } else {
+//   alert("i numeri sono uguali");
+// }
+// // random
+// const numeroRandom =
+//   Math.floor(Math.random() * (numeroPiuGrande - numeroPiuPiccolo)) +
+//   numeroPiuPiccolo;
+
+// //   stampa
+// if (isNaN(numeroRandom)) {
+//   alert("Riprova");
+// } else {
+//   alert("Ecco il numero: " + numeroRandom);
+// }
+
+// es 2
+
+// const primaParola = prompt("inserisci la prima parola");
+// const secondaParola = prompt("inserisci la seconda parola");
+
+// if (primaParola.length == secondaParola.length) {
+//   alert(primaParola + " " + secondaParola);
+// } else if (primaParola.length < secondaParola.length) {
+//   alert(secondaParola);
+// } else {
+//   alert(primaParola);
+// }
+
+// ES 3
+
+const container = [];
+
+numero = 0;
+
+do {
+  const numeroTemporaneo = parseInt(prompt("inserisci un numero"));
+  container.push(numeroTemporaneo);
+  numero += numeroTemporaneo;
+} while (numero < 50);
+{
+  const numeroTemporaneo = parseInt(prompt("inserisci un numero"));
+  container.push(numeroTemporaneo);
+  numero += numeroTemporaneo;
 }
-// random
-const NumeroRandom =
-  Math.floor(Math.random() * NumeroPiuGrande) + NumeroPiuPiccolo;
 
-if (isNaN(NumeroRandom)) {
-  alert("Riprova");
-} else {
-  alert("Ecco il numero: " + NumeroRandom);
-}
+console.log(numero);
+console.log(container);
--- readme.md
+++ readme.md
@@ -8,3 +8,24 @@ _Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale co
 - Chiedi all'utente un _SecondoNumero_ ;
 - Controlla il _NumeroPiùGrande_ e assegnalo ;
 - Crea un numero random che sia compreso tra i due numeri ;
+
+# TRACCIA 2
+
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+# ISTRUZIONI
+
+- Chiedi all'utente una _parola_ ;
+- Chiedi all'utente un'altra _parola_ ;
+
+**SE**
+
+- Sono uguali stampale ;
+
+**ALTRIMENTI**
+
+- Stampa la più lunga ;
+
+# TRACCIA 3
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
