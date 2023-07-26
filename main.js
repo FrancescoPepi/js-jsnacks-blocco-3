0f6e24a949e0a428c355b909d54781cde8719563
FrancescoPepi <francescopepi16@gmail.com>
2023-07-26 15:32:52 +0200

    Risultato

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ const PrimoNumero = parseInt(prompt("inserisci il primo numero"));
 const SecondoNumero = parseInt(prompt("inserisci il secondo numero"));
 
 let NumeroPiuGrande;
+let NumeroPiuPiccolo;
 
 if (PrimoNumero > SecondoNumero) {
   NumeroPiuGrande = PrimoNumero;
+  NumeroPiuPiccolo = SecondoNumero;
 } else if (PrimoNumero < SecondoNumero) {
   NumeroPiuGrande = SecondoNumero;
+  NumeroPiuPiccolo = PrimoNumero;
 } else {
   allert("i numeri sono uguali");
 }
 
-const NumeroRandom = Math.floor(Math.random() * NumeroPiuGrande);
+const NumeroRandom =
+  Math.floor(Math.random() * NumeroPiuGrande) + NumeroPiuPiccolo;
+
+alert(NumeroRandom);
